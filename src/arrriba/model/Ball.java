24da5f5d6b9615189dca07d405f74c5b06907cfb
Twyxt95 <a.schmidt0101@web.de>
2016-05-30 21:48:38 +0200

    kollision
    math3

--- src/arrriba/model/Ball.java
+++ src/arrriba/model/Ball.java
@@ -12,6 +12,12 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import javafx.scene.paint.Paint;
 import javafx.scene.shape.Circle;
+import org.apache.commons.math3.linear.Array2DRowRealMatrix;
+import org.apache.commons.math3.linear.ArrayRealVector;
+import org.apache.commons.math3.linear.DecompositionSolver;
+import org.apache.commons.math3.linear.LUDecomposition;
+import org.apache.commons.math3.linear.RealMatrix;
+import org.apache.commons.math3.linear.RealVector;
 
 /**
  *
@@ -143,7 +149,6 @@ public class Ball extends GameModel {
                 cornerPoints=obstacle.getCornerPoints();
                 
                 for(int c=0;c<=cornerPoints.length-3;c=c+2){
-                    System.out.println(c);
                     double a=cornerPoints[c]-cornerPoints[c+2];
                     double b=cornerPoints[c+1]-cornerPoints[c+3];
                     gX.add(a);
@@ -152,23 +157,62 @@ public class Ball extends GameModel {
                     ngY.add(gX.get(gX.size()-1));
                     double e= VectorCalculation.times(ngX.get(ngX.size()-1), ngY.get(ngY.size()-1), getStartX()-cornerPoints[c], getStartY()-cornerPoints[c+1]);
                     double d= Math.abs(e)/VectorCalculation.abs(ngX.get(ngX.size()-1), ngY.get(ngY.size()-1));
-                    hit.add(d/Math.abs(getVX()));
-                    System.out.println(hit.get(hit.size()-1));
+                    double x=-(cornerPoints[c+2]-cornerPoints[c]);
+                    if((cornerPoints[c+2]-cornerPoints[c])==0){
+                        x=(cornerPoints[c+2]-cornerPoints[c]);
+                    }
+                    RealMatrix coefficients =
+                    new Array2DRowRealMatrix(new double[][] { { getVX()-getStartX(),x}, { getVY()-getStartY(),-(cornerPoints[c+3]-cornerPoints[c+1])} },
+                       false);
+                    DecompositionSolver solver = new LUDecomposition(coefficients).getSolver();
+                    RealVector constants = new ArrayRealVector(new double[] { cornerPoints[c]-getStartX(),cornerPoints[c+1]-getStartY()}, false);
+                    RealVector solution = solver.solve(constants);                    
+                    //double collX=cornerPoints[c]+solution.getEntry(1)*(cornerPoints[c+2]-cornerPoints[c]);
+                    double collX=getStartX()+solution.getEntry(0)*(getVX()-getStartX());
+                    System.out.println(collX + "collX");
+                    System.out.println(cornerPoints[c+2]);
+                    System.out.println(cornerPoints[c]);
+                    //double collY=cornerPoints[c+1]+solution.getEntry(1)*(cornerPoints[c+3]-cornerPoints[c+1]);
+                    double collY=getStartY()+solution.getEntry(0)*(getVY()-getStartY());
+                    System.out.println(constants.getEntry(0)+ " " + constants.getEntry(1));
+                    System.out.println(solution.getEntry(0)+"w0");
+                    System.out.println(solution.getEntry(1));
+                    System.out.println(collY + "collY");
+                    System.out.println(cornerPoints[c+3]);
+                    System.out.println(cornerPoints[c+1]);
+                    System.out.println(getVX() + "vx");
+                    System.out.println(getVY() + "vy");
+                    System.out.println(getStartX() + "sx");
+                    System.out.println(getStartY() + "sy");
+                    for(int k=0;k<=1;k++){
+                        for(int i=0;i<=1;i++){
+                            System.out.println(coefficients.getRowVector(k).getEntry(i));
+                        }
+                    }
+                    
+                    
+                    if(collX>=cornerPoints[c+2] && collX<=cornerPoints[c] && collY>=cornerPoints[c+3] && collY<=cornerPoints[c+1]){
+                       hit.add(d/Math.abs(getVX())); 
+                       System.out.println(hit.get(hit.size()-1));
+                    }       
+                    //System.out.println(hit.get(hit.size()-1));
+                    
                 }
-            double low=hit.get(0);          
-            for(int i=0; i<=hit.size()-1; i++){
-                if(hit.get(i)<low){
-                    low=hit.get(i);
-                    lowIndex=i;
+            if(hit.size()>0){    
+                double low=hit.get(0);          
+                for(int i=0; i<=hit.size()-1; i++){
+                    if(hit.get(i)<low){
+                        low=hit.get(i);
+                        lowIndex=i;
+                    }
                 }
-            }
-            lowIndex=hit.size()-1;
+                lowIndex=hit.size()-1;
             System.out.println(VectorCalculation.abs(gX.get(lowIndex), gY.get(lowIndex))+"bumm");
-            
+            }                        
             punch=false;
         }        
         
-        if(zeroCounter>=1){
+        if(zeroCounter>=1 && hit.size()>0){
             if(t>=hit.get(lowIndex)){
                 setLastHit(t);
                 //punch=true;
