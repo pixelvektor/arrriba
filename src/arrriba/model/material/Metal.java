484b2f0a3327d1105014ce08b825fe9fb5e9f56f
LaMaSi <lara@sievers-home.de>
2016-06-12 18:26:38 +0200

    Ein Zahl war falsch...

--- src/arrriba/model/material/Metal.java
+++ src/arrriba/model/material/Metal.java
@@ -12,7 +12,7 @@ package arrriba.model.material;
 public class Metal extends Material {
     private static final String NAME = "Metall";
     private double density = 7.8;
-    private double frictionCoefficient = 20.9; // wert bezieht sich auf Stein, evtl ändern
+    private double frictionCoefficient = 0.9; // wert bezieht sich auf Stein, evtl ändern
     private String texturePath = "/arrriba/view/metallkugel.png";
     
 
